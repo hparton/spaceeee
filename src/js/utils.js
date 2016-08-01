afca05a52a6085fc5a1123d14df700e10a548cfb
Harry Parton <hparton@hotmail.co.uk>
2016-08-01 15:23:48 +0100

    Create a helper to generate separate canvas elements

---
+++ src/js/utils.js
@@ -0,0 +1,27 @@
+export const createChildCanvas = function(id, parentCanvas, order, background) {
+  	let canvas = document.createElement("canvas");
+  	let parent = parentCanvas;
+    let ctx = canvas.getContext("2d");
+
+    canvas.id = id;
+
+    // Make the height/width/position match the parents.
+    canvas.width = parentCanvas.width;
+  	canvas.height = parentCanvas.height;
+  	canvas.position = 'absolute';
+  	canvas.style.left = '0';
+  	canvas.style.top = '0';
+
+  	// Set z-index based on order.
+
+  	if (background) {
+	  	canvas.style.background = background;
+  	}
+
+  	parent.parentNode.insertBefore(canvas, parent)
+
+  	return {
+  		canvas: canvas,
+  		ctx: ctx
+  	}
+}
