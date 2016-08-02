577ba8634dacbb077399a87364e30baef3b95c14
Harry Parton <hparton@hotmail.co.uk>
2016-08-02 11:08:32 +0100

    Add background argument to starField

--- src/js/stars.js
+++ src/js/stars.js
@@ -1,14 +1,13 @@
 import { createChildCanvas } from './utils';
 
-export const StarField = function(canvas) {
+export const StarField = function(canvas, background) {
 	this.parentCanvas = canvas;
-  	this.background = '#251b4f';
+  	this.background = background;
 }
 
 StarField.prototype.init = function() {
-	// Make a new one
+	// Make a new canvas
   	this.el = createChildCanvas('star-field', this.parentCanvas, -3, this.background);
-
   	this.drawStarField(200)
 }
 
@@ -18,19 +17,19 @@ StarField.prototype._drawStar = function (ctx) {
 	var radius = Math.random() * 2;
 	var brightness = random(80, 100) / 100;
 
-    // start drawing the star
     ctx.beginPath();
     // set the brightness of the star
     ctx.globalAlpha = brightness;
     ctx.fillStyle = "#ffffff";
-    // draw the star (an arc of radius 2 * pi)
+    // draw the star
     ctx.arc(x, y, radius, 0, Math.PI * 2, true);
-    // fill the star and stop drawing it
+    // fill the star
     ctx.fill();
     ctx.closePath();
 }
 
 StarField.prototype.drawStarField = function (total) {
+	// Many stars. Such field.
 	for (let i = 0; i <= total; i++) {
 		this._drawStar(this.el.ctx)
 	}
